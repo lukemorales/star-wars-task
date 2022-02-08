0c179c69775b0cf6037b773764fb1ac138d614c1
Luke Morales <lukemorales@live.com>
2022-02-08 20:40:54 -0300

    Create typed useAppDispatch and useAppSelector redux hooks

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,11 +5,8 @@ import { planetsApi } from '../queries';
 
 export const store = configureStore({
   reducer: {
-    // Add the generated reducer as a specific top-level slice
     [planetsApi.reducerPath]: planetsApi.reducer,
   },
-  // Adding the api middleware enables caching, invalidation, polling,
-  // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(planetsApi.middleware),
 });
@@ -17,3 +14,6 @@ export const store = configureStore({
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
 // see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
 setupListeners(store.dispatch);
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
