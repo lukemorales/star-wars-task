2f9c5193324d3eff82c355c3f3eb0ef4b1f47da2
Luke Morales <lukemorales@live.com>
2022-02-09 10:44:40 -0300

    Transform planets query response to get planets ids from url

--- src/queries/planets.ts
+++ src/queries/planets.ts
@@ -1,13 +1,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-import type { Planet } from '../types';
+import type { Planet, PlanetWithId, SwapiAPIResponse } from '../types';
 
 export const planetsApi = createApi({
   reducerPath: 'planetsApi',
   baseQuery: fetchBaseQuery({ baseUrl: 'https://swapi.dev/api/' }),
   endpoints: (builder) => ({
-    getPlanets: builder.query<Planet[], string>({
+    getPlanets: builder.query<SwapiAPIResponse<PlanetWithId>, void | null>({
       query: () => `planets`,
+      transformResponse: (response: SwapiAPIResponse<Planet>) => ({
+        ...response,
+        results: response.results.map((planet) => ({
+          ...planet,
+          id:
+            planet.url.match(/.*\/(?<id>[0-9])+./)?.groups?.id ||
+            `${Math.random()}`,
+        })),
+      }),
     }),
   }),
 });
