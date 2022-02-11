276755a3b344d47c491ef4e9e5cbda0872b5d45e
Luke Morales <lukemorales@live.com>
2022-02-11 00:13:25 -0300

    Improve Zod validation on PlanetModal form

--- src/components/PlanetModal/index.tsx
+++ src/components/PlanetModal/index.tsx
@@ -9,9 +9,11 @@ import Input from '../Input';
 import Select from '../Select';
 
 const formSchema = z.object({
-  name: z.string({
-    required_error: 'Name is required',
-  }),
+  name: z
+    .string({
+      required_error: 'Name is required',
+    })
+    .min(1),
   rotation_period: z.number({
     required_error: 'Rotation Period is required',
     invalid_type_error: 'Rotation Period must be a number',
@@ -24,15 +26,21 @@ const formSchema = z.object({
     required_error: 'Diameter is required',
     invalid_type_error: 'Diameter must be a number',
   }),
-  climate: z.string({
-    required_error: 'Climate is required',
-  }),
-  gravity: z.string({
-    required_error: 'Gravity is required',
-  }),
-  terrain: z.string({
-    required_error: 'Terrain is required',
-  }),
+  climate: z
+    .string({
+      required_error: 'Climate is required',
+    })
+    .min(1),
+  gravity: z
+    .string({
+      required_error: 'Gravity is required',
+    })
+    .min(1),
+  terrain: z
+    .string({
+      required_error: 'Terrain is required',
+    })
+    .min(1),
   surface_water: z.number({
     required_error: 'Surface Water is required',
     invalid_type_error: 'Surface Water must be a number',
